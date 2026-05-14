1f1c786079f7cbe794ee9ef59bb51864072015fb
ch3p4ll3 <slinucs@gmail.com>
2026-05-14 11:49:29 +0200

    Refactor ADC and SampleBuffer structures; update packet handling and remove unused CRC dependency

--- include/struct.h
+++ include/struct.h
@@ -1,19 +1,44 @@
 #include <stdint.h>
 
-struct __attribute__((__packed__)) ADC_Packet {
-    uint8_t header1 = 0xAA;
-    uint8_t header2 = 0xBB;
-    int32_t ch0;
-    int32_t ch1;
-    int32_t ch2;
-    uint32_t crc;
-};
+#define ANYSHAKE_HEADER_1     0x01
+#define ANYSHAKE_HEADER_2     0xFE
+#define ANYSHAKE_TRAILER_1    0xEF
+#define ANYSHAKE_TRAILER_2    0x10
 
+#define DEFAULT_SAMPLE_RATE       100
+#define DEFAULT_ADC_GAIN_INDEX    0
+#define DEFAULT_ADC_DRATE_INDEX   8
+#define DEVICE_ID                 0x00000001
 
-struct __attribute__((__packed__)) SettingsPacket {
-    uint8_t header1 = 0xCC;
-    uint8_t header2 = 0xDD;
-    uint16_t samplingSpeed;
-    uint8_t ADCGain;
-    uint8_t ADCDataRate;
+#define PACKET_INTERVAL_MS    100
+#define CHUNK_LENGTH          ((PACKET_INTERVAL_MS * DEFAULT_SAMPLE_RATE) / 1000)
+
+#define CONFIG_PACKET_INTERVAL_100MS  0x00
+#define CONFIG_SAMPLE_RATE_100HZ      0x03
+#define CONFIG_GNSS_NOT_AVAILABLE     0x00
+#define CONFIG_CHANNEL_INT32          0x03
+#define CONFIG_CHANNEL_DISABLED       0x00
+
+#define HEADER_SIZE      2
+#define TIMESTAMP_SIZE   8
+#define DEVICE_CONFIG_SIZE 4
+#define VARIABLE_DATA_SIZE 4
+#define CHECKSUM_SIZE    1
+#define TRAILER_SIZE     2
+
+#define CHANNEL_DATA_SIZE  (CHUNK_LENGTH * 3 * sizeof(int32_t))
+
+#define PACKET_SIZE        (HEADER_SIZE + TIMESTAMP_SIZE + DEVICE_CONFIG_SIZE + VARIABLE_DATA_SIZE + CHANNEL_DATA_SIZE + CHECKSUM_SIZE + TRAILER_SIZE)
+
+// Set via build_flags, e.g. -D UNIX_EPOCH_MS=1740000000000ULL
+// Defaults to 0: timestamps count ms from boot. Set to real epoch for proper time.
+#ifndef UNIX_EPOCH_MS
+#define UNIX_EPOCH_MS 0ULL
+#endif
+
+struct __attribute__((__packed__)) SampleBuffer {
+    int32_t ch0[CHUNK_LENGTH];
+    int32_t ch1[CHUNK_LENGTH];
+    int32_t ch2[CHUNK_LENGTH];
+    uint8_t count;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,259 +1,186 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
-#include <CRC.h>
 #include <ADS1256.h>
 
 #include <struct.h>
-#include <enums.h>
-
 
 #if defined(ARDUINO_ARCH_RP2040)
-  #if defined(USE_SPI1)
-    #define SPI_MOSI 11
-    #define SPI_MISO 12
-    #define SPI_SCK  10
-    #define USE_SPI  SPI1
-    ADS1256 A(18, 20, 21, 19, 2.500, &USE_SPI);  // RP2040 Zero - SPI1
-  #else
-    #define SPI_MOSI 3
-    #define SPI_MISO 4
-    #define SPI_SCK  2
-    #define USE_SPI  SPI
-    ADS1256 A(7, ADS1256::PIN_UNUSED, 6, 5, 2.500, &USE_SPI);  // RP2040 Waveshare Mini - SPI0
-  #endif
-
-#elif defined(ARDUINO_ARCH_STM32)
-  #if defined(USE_SPI2)
-    #define USE_SPI spi2
-    SPIClass spi2(PB15, PB14, PB13);
-    ADS1256 A(PB10, PB11, ADS1256::PIN_UNUSED, PB12, 2.500, &USE_SPI);  // STM32 SPI2
-  #else
-    #define USE_SPI SPI
-    ADS1256 A(PA2, ADS1256::PIN_UNUSED, ADS1256::PIN_UNUSED, PA4, 2.500, &USE_SPI);  // STM32 SPI1
-  #endif
-
-#elif defined(TEENSYDUINO)
-  #if defined(USE_SPI2)
-    #define USE_SPI SPI2
-  #elif defined(USE_SPI1)
-    #define USE_SPI SPI1
-  #else
-    #define USE_SPI SPI
-  #endif
-  ADS1256 A(7, ADS1256::PIN_UNUSED, 8, 10, 2.500, &USE_SPI);  // Teensy 4.0 / 4.1
-
-#elif defined(ARDUINO_ARCH_ESP32)
-  SPIClass hspi(HSPI);
-  #if defined(USE_HSPI)
-    #define USE_SPI hspi
-    // begin() called in setup() — see below
-  #else
-    #define USE_SPI SPI
-  #endif
-  ADS1256 A(16, 17, ADS1256::PIN_UNUSED, 15, 2.500, &USE_SPI);  // ESP32 WROOM
-  #ifndef LED_BUILTIN
-    #define LED_BUILTIN 2   // most ESP32 boards have the onboard LED on GPIO2
-  #endif
-
-#else  // AVR fallback
-  #define SPI_MOSI MOSI
-  #define SPI_MISO MISO
-  #define SPI_SCK  SCK
-  #define USE_SPI  SPI
-  ADS1256 A(2, ADS1256::PIN_UNUSED, 8, 10, 2.500, &USE_SPI);  // Uno / Nano
+#if defined(USE_SPI1)
+#define SPI_MOSI 11
+#define SPI_MISO 12
+#define SPI_SCK 10
+#define USE_SPI SPI1
+ADS1256 A(18, 20, 21, 19, 2.500, &USE_SPI);
+#else
+#define SPI_MOSI 3
+#define SPI_MISO 4
+#define SPI_SCK 2
+#define USE_SPI SPI
+ADS1256 A(7, ADS1256::PIN_UNUSED, 6, 5, 2.500, &USE_SPI);
 #endif
 
-// default settings
-#define SAMPLING_SPEED 100  // in Hz
-#define TIMEOUT_DURATION 1000 // in milliseconds
-#define ADC_PACKET_PAYLOAD_LEN  (sizeof(ADC_Packet) - sizeof(uint32_t))
-#define WRITE_TIMEOUT 500 // ms to detect a "stuck" serial port
-// #define DEBUG_MODE
+#elif defined(ARDUINO_ARCH_STM32)
+#if defined(USE_SPI2)
+#define USE_SPI spi2
+SPIClass spi2(PB15, PB14, PB13);
+ADS1256 A(PB10, PB11, ADS1256::PIN_UNUSED, PB12, 2.500, &USE_SPI);
+#else
+#define USE_SPI SPI
+ADS1256 A(PA2, ADS1256::PIN_UNUSED, ADS1256::PIN_UNUSED, PA4, 2.500, &USE_SPI);
+#endif
+
+#elif defined(TEENSYDUINO)
+#if defined(USE_SPI2)
+#define USE_SPI SPI2
+#elif defined(USE_SPI1)
+#define USE_SPI SPI1
+#else
+#define USE_SPI SPI
+#endif
+ADS1256 A(7, ADS1256::PIN_UNUSED, 8, 10, 2.500, &USE_SPI);
+
+#elif defined(ARDUINO_ARCH_ESP32)
+SPIClass hspi(HSPI);
+#if defined(USE_HSPI)
+#define USE_SPI hspi
+#else
+#define USE_SPI SPI
+#endif
+ADS1256 A(16, 17, ADS1256::PIN_UNUSED, 15, 2.500, &USE_SPI);
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+
+#else
+#define SPI_MOSI MOSI
+#define SPI_MISO MISO
+#define SPI_SCK SCK
+#define USE_SPI SPI
+ADS1256 A(2, ADS1256::PIN_UNUSED, 8, 10, 2.500, &USE_SPI);
+#endif
 
 unsigned long lastSampleTime = 0;
-unsigned long interval = 1000000 / SAMPLING_SPEED; // 1_000_000us / 100Hz = 10ms
+unsigned long interval = 1000000 / DEFAULT_SAMPLE_RATE;
 
-unsigned long lastHeartbeat = 0;
+SampleBuffer buf;
 
-SystemState currentState = SystemState::STOP;
-SettingsPacket incomingSettings;
-static unsigned long bufferFullStartTime;
-static bool isTimerActive = false; // Separate flag for the "Zero" edge case
+uint8_t pgaSettings[7] = {
+    PGA_1, PGA_2, PGA_4, PGA_8,
+    PGA_16, PGA_32, PGA_64};
 
-uint8_t PGASettings[7] = {
-  PGA_1,
-  PGA_2,
-  PGA_4,
-  PGA_8,
-  PGA_16,
-  PGA_32,
-  PGA_64
-};
+uint8_t dataRateSettings[16] = {
+    DRATE_2SPS, DRATE_5SPS, DRATE_10SPS, DRATE_15SPS,
+    DRATE_25SPS, DRATE_30SPS, DRATE_50SPS, DRATE_60SPS,
+    DRATE_100SPS, DRATE_500SPS, DRATE_1000SPS, DRATE_2000SPS,
+    DRATE_3750SPS, DRATE_7500SPS, DRATE_15000SPS, DRATE_30000SPS};
 
-uint8_t DataRateSettings[16] = {
-  DRATE_2SPS,
-  DRATE_5SPS,
-  DRATE_10SPS,
-  DRATE_15SPS,
-  DRATE_25SPS,
-  DRATE_30SPS,
-  DRATE_50SPS,
-  DRATE_60SPS,
-  DRATE_100SPS,
-  DRATE_500SPS,
-  DRATE_1000SPS,
-  DRATE_2000SPS,
-  DRATE_3750SPS,
-  DRATE_7500SPS,
-  DRATE_15000SPS,
-  DRATE_30000SPS
-};
+static uint32_t build_device_config()
+{
+  return (CONFIG_PACKET_INTERVAL_100MS << 30) |
+         (CONFIG_SAMPLE_RATE_100HZ << 27) |
+         (CONFIG_GNSS_NOT_AVAILABLE << 26) |
+         (CONFIG_CHANNEL_INT32 << 24) |
+         (CONFIG_CHANNEL_INT32 << 22) |
+         (CONFIG_CHANNEL_INT32 << 20) |
+         (CONFIG_CHANNEL_DISABLED << 18) |
+         (CONFIG_CHANNEL_DISABLED << 16) |
+         (CONFIG_CHANNEL_DISABLED << 14) |
+         (CONFIG_CHANNEL_DISABLED << 12) |
+         (CONFIG_CHANNEL_DISABLED << 10) |
+         (1 << 4) |
+         (1 << 0);
+}
 
+static void send_anyshake_packet()
+{
+  uint8_t packet[PACKET_SIZE];
 
-void initADC(SettingsPacket *s);
-void getSettings();
-void validateSettings(SettingsPacket *s);
-void sendPacket();
+  packet[0] = ANYSHAKE_HEADER_1;
+  packet[1] = ANYSHAKE_HEADER_2;
 
+  int64_t timestamp = (int64_t)millis();
+  memcpy(&packet[2], &timestamp, TIMESTAMP_SIZE);
 
-void setup() {
-  Serial.begin(115200);  //The value does not matter if you use an MCU with native USB
+  uint32_t device_config = build_device_config();
+  memcpy(&packet[10], &device_config, DEVICE_CONFIG_SIZE);
 
-#if defined(ARDUINO_ARCH_RP2040)  //If RP2040 is used, we need to pass the SPI pins
+  uint32_t var_uint = 0;
+  float var_float = 0.0f;
+  switch ((millis() / 1000) % 10)
+  {
+  case 0:
+    var_uint = DEVICE_ID;
+    memcpy(&packet[14], &var_uint, VARIABLE_DATA_SIZE);
+    break;
+  default:
+    var_uint = 0;
+    memcpy(&packet[14], &var_uint, VARIABLE_DATA_SIZE);
+    break;
+  }
+
+  uint16_t offset = HEADER_SIZE + TIMESTAMP_SIZE + DEVICE_CONFIG_SIZE + VARIABLE_DATA_SIZE;
+  memcpy(&packet[offset], buf.ch0, CHUNK_LENGTH * sizeof(int32_t));
+  memcpy(&packet[offset + CHUNK_LENGTH * sizeof(int32_t)], buf.ch1, CHUNK_LENGTH * sizeof(int32_t));
+  memcpy(&packet[offset + 2 * CHUNK_LENGTH * sizeof(int32_t)], buf.ch2, CHUNK_LENGTH * sizeof(int32_t));
+
+  uint16_t chk_start = HEADER_SIZE;
+  uint16_t chk_end = PACKET_SIZE - TRAILER_SIZE - CHECKSUM_SIZE;
+  uint8_t checksum = 0;
+  for (uint16_t i = chk_start; i < chk_end; i++)
+  {
+    checksum ^= packet[i];
+  }
+  packet[PACKET_SIZE - TRAILER_SIZE - CHECKSUM_SIZE] = checksum;
+
+  packet[PACKET_SIZE - TRAILER_SIZE] = ANYSHAKE_TRAILER_1;
+  packet[PACKET_SIZE - 1] = ANYSHAKE_TRAILER_2;
+
+  Serial.write(packet, PACKET_SIZE);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+#if defined(ARDUINO_ARCH_RP2040)
   SPI.setSCK(SPI_SCK);
   SPI.setTX(SPI_MOSI);
   SPI.setRX(SPI_MISO);
 #endif
 
-#if defined(USE_HSPI)      //If ESP32 is used, we need to start SPI with a non-strapping MISO pin
-  hspi.begin(14, 25, 13);  //SCK, MISO (safe), MOSI
+#if defined(USE_HSPI)
+  hspi.begin(14, 25, 13);
 #endif
 
-  getSettings();
+  A.InitializeADC();
+  A.setPGA(pgaSettings[DEFAULT_ADC_GAIN_INDEX]);
+  A.setDRATE(dataRateSettings[DEFAULT_ADC_DRATE_INDEX]);
+
+  buf.count = 0;
 }
 
-void loop() {
-  // Check for Settings/Disconnect via Serial availability
-  if (Serial.available() > 0) {
-    // If we see the settings header, update settings
-    if (Serial.peek() == 0xCC) {
-      getSettings();
-      lastSampleTime = micros();
-      currentState = SystemState::STREAMING;
-    } else {
-      // Clear unexpected bytes
-      Serial.read();
+void loop()
+{
+  unsigned long now = micros();
+  if (now - lastSampleTime >= interval)
+  {
+    lastSampleTime += interval;
+
+    int32_t val0 = A.cycleDifferential();
+    int32_t val1 = A.cycleDifferential();
+    int32_t val2 = A.cycleDifferential();
+    A.cycleDifferential();
+
+    buf.ch0[buf.count] = val0;
+    buf.ch1[buf.count] = val1;
+    buf.ch2[buf.count] = val2;
+    buf.count++;
+
+    if (buf.count >= CHUNK_LENGTH)
+    {
+      send_anyshake_packet();
+      buf.count = 0;
     }
   }
-
-  if (Serial.availableForWrite() < 32) {
-      if (!isTimerActive) {
-          bufferFullStartTime = millis();
-          isTimerActive = true;
-      }
-
-      if (millis() - bufferFullStartTime > WRITE_TIMEOUT) {
-          if (currentState != SystemState::STOP) {
-              currentState = SystemState::STOP;
-              A.stopConversion();
-          }
-      }
-  } 
-  else {
-      isTimerActive = false; // Reset the flag
-  }
-
-  // Sampling and Streaming
-  if (currentState == SystemState::STREAMING) {
-    unsigned long currentTime = micros();
-    // Use >= to handle potential slight jitter
-    if (currentTime - lastSampleTime >= interval) {
-      sendPacket();
-    }
-  } else {
-    // We are in STOP. Maybe blink an LED to show "Waiting for PC" ?
-    digitalWrite(LED_BUILTIN, (millis() / 500) % 2); 
-  }
-}
-
-void initADC(SettingsPacket *s) {
-  A.InitializeADC();  //See the documentation for every details
-
-  // Set PGA
-  A.setPGA(PGASettings[s->ADCGain]);
-  //Set DRATE
-  A.setDRATE(DataRateSettings[s->ADCDataRate]);
-}
-
-void getSettings() {
-  bool settingsReceived = false;
-
-  while (!settingsReceived) {
-    if (Serial.available() >= sizeof(SettingsPacket)) {
-      if (Serial.peek() == 0xCC) {
-        Serial.readBytes((uint8_t*)&incomingSettings, sizeof(SettingsPacket));
-
-        if (incomingSettings.header1 == 0xCC && incomingSettings.header2 == 0xDD) {
-          // Validate and apply settings
-          validateSettings(&incomingSettings);
-          // Respond with the same structure for verification
-          Serial.write((uint8_t*)&incomingSettings, sizeof(incomingSettings));
-          //Serial.flush();
-          
-          settingsReceived = true;
-          currentState = SystemState::STREAMING;
-        }
-      } else {
-        Serial.read(); // Discard garbage
-      }
-    }
-  }
-}
-
-void validateSettings(SettingsPacket *s){
-  if (s->samplingSpeed > 0) {
-    interval = 1000000UL / s->samplingSpeed; 
-  }
-
-  // Update ADC Gain Index (Validate range 0-6)
-  if (s->ADCGain > 6) {
-    s->ADCGain = 6; // Default to index 6 (PGA_64)
-  }
-
-  // Update Data Rate Index (Validate range 0-15)
-  if (s->ADCDataRate > 15) {
-    s->ADCDataRate = 11; // Default to index 14 (2000SPS)
-  }
-
-  #ifndef DEBUG_MODE
-    initADC(s);
-  #endif
-}
-
-void sendPacket() {
-  lastSampleTime += interval; // Schedule next sample time based on the previous one to maintain consistent intervals, even if there is some processing delay
-
-  ADC_Packet frame;
-
-  frame.header1 = 0xAA;
-  frame.header2 = 0xBB;
-  
-  #ifndef DEBUG_MODE
-    frame.ch0 = A.cycleDifferential();
-    frame.ch1 = A.cycleDifferential();
-    frame.ch2 = A.cycleDifferential();
-
-    A.cycleDifferential(); // we don't need the last channel but we need to call it to update the MUX for the next cycle
-  #else
-    frame.ch0 = random(-10, 10); //A.cycleDifferential();
-    frame.ch1 = random(-10, 10); //A.cycleDifferential();
-    frame.ch2 = random(-10, 10); //A.cycleDifferential();
-  #endif
-
-  // Calculate Checksum
-  // CRC-32 (ISO 3309) over header + channel data (all bytes before the crc field).
-  frame.crc = calcCRC32((uint8_t *)&frame, ADC_PACKET_PAYLOAD_LEN);
-
-  Serial.write((uint8_t*)&frame, sizeof(frame));  // Send the entire frame as binary data
-  //Serial.flush(); // Ensure all data is sent before proceeding
 }
